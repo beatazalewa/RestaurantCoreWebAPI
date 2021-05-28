97deca3eff141d950d585093390d4bb425b21116
Beata Zalewa <info@zalnet.pl>
2021-05-28 14:42:00 +0200

    Added a folder Models and a model class - Restaurant

---
+++ RestaurantCoreWebAPI.Server/Models/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantCoreWebAPI.Server.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        [Display(Name = "Restaurant name:")]
+        public string Name { get; set; }
+        [Display(Name = "Address:")]
+        public string Address { get; set; }
+        [Display(Name = "Is open:")]
+        public bool IsOpen { get; set; }
+        [Display(Name = "Speciality:")]
+        public string Speciality { get; set; }
+        [Range(0, 5)]
+        [Display(Name = "Review:")]
+        public int Review { get; set; }
+    }
+}
+
+
